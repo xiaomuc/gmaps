a09189a57ac6a1be6014a1c792392487d5169ee0
PB1 小木 次郎 <1027541@knet.nttdata-kansai.co.jp>
2019-06-06 15:58:24 +0900

    marker

---
+++ maps-marker.js
@@ -0,0 +1,79 @@
+//  Google Mapsオブジェクト 
+var map;
+//  GeoCoder（住所等から位置を特定するAPI)
+var geocoder;
+//  地図スケール判定用の変数
+var minLng, maxLng, minLat, maxLat;
+
+//  地図初期化用のコールバック
+function initMap() {
+    //マップの生成と初期位置
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: {
+            lat: 32.803198,
+            lng: 130.707875
+        }
+    });
+    //GeoCoder生成
+    geocoder = new google.maps.Geocoder();
+
+    //placesオブジェクト(places.js)に登録された位置からマーカーを生成する
+    for (var i = 0; i < places.length; i++) {
+        setMarker(places[i]);
+    }
+}
+
+//  表示位置最適化:マーカーの位置から地図サイズ・表示位置を最適化する
+function fitmap(location) {
+    var lat = location.lat();
+    var lng = location.lng();
+    if (minLng == undefined || minLng > lng) {
+        minLng = lng
+    }
+    if (maxLng == undefined || maxLng < lng) {
+        maxLng = lng;
+    }
+    if (minLat == undefined || minLat > lat) {
+        minLat = lat;
+    }
+    if (maxLat == undefined || maxLat < lat) {
+        maxLat = lat;
+    }
+    var sw = new google.maps.LatLng(maxLat, minLng);
+    var ne = new google.maps.LatLng(minLat, maxLng);
+    var bounds = new google.maps.LatLngBounds(sw, ne);
+    map.fitBounds(bounds);
+}
+
+//  マーカー生成：位置情報から地図上にマーカーを設定し、情報ウィンドウを生成する
+function setMarker(place) {
+    //  住所などから位置を特定
+    geocoder.geocode({
+        'address': place.address
+    }, function(results, status) {
+        //正常に位置が取得さた場合
+        if (status == google.maps.GeocoderStatus.OK) {
+            //情報ウィンドウ生成
+            var infowindow = new google.maps.InfoWindow({
+                content: "<h1><a href='" +
+                    place.url + "'>" + place.title + "</a></h1><p>" + place.description + "</p>"
+            });
+            //マーカー生成
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                animation: google.maps.Animation.DROP,
+                icon: place.icon,
+                map: map
+            });
+            //クリックイベントで情報ウィンドウを開く
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
+            //表示位置最適化
+            fitmap(results[0].geometry.location);
+        } else {
+            alert(status);
+        }
+    });
+}
