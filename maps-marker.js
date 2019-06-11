949a740d187ca4f38c3eea5fc47d4de37096cd53
PB1 小木 次郎 <1027541@knet.nttdata-kansai.co.jp>
2019-06-11 17:40:47 +0900

    markers

--- maps-marker.js
+++ maps-marker.js
@@ -4,7 +4,7 @@ var map;
 var geocoder;
 //  地図スケール判定用の変数
 var minLng, maxLng, minLat, maxLat;
-
+var infowindow;
 //  地図初期化用のコールバック
 function initMap() {
     //マップの生成と初期位置
@@ -17,11 +17,15 @@ function initMap() {
     });
     //GeoCoder生成
     geocoder = new google.maps.Geocoder();
+    infowindow = new google.maps.InfoWindow();
 
     //placesオブジェクト(places.js)に登録された位置からマーカーを生成する
     for (var i = 0; i < places.length; i++) {
         setMarker(places[i]);
     }
+    google.maps.event.addListener(map, "click", function(event) {
+        infowindow.close();
+    });
 }
 
 //  表示位置最適化:マーカーの位置から地図サイズ・表示位置を最適化する
@@ -55,19 +59,25 @@ function setMarker(place) {
         //正常に位置が取得さた場合
         if (status == google.maps.GeocoderStatus.OK) {
             //情報ウィンドウ生成
-            var infowindow = new google.maps.InfoWindow({
+            var icon_path = "http://maps.google.com/mapfiles/ms/icons/" + place.icon + ".png";
+            /*var infowindow = new google.maps.InfoWindow({
                 content: "<h1><a href='" +
-                    place.url + "'>" + place.title + "</a></h1><p>" + place.description + "</p>"
-            });
+                    place.url + "'>" + place.title + "</a></h1><p>" + place.description + "</p>" +
+                    "<p>icon: <img src='" + icon_path + "'><strong>" + place.icon + "</strong></p>"
+            });*/
             //マーカー生成
             var marker = new google.maps.Marker({
                 position: results[0].geometry.location,
                 animation: google.maps.Animation.DROP,
-                icon: place.icon,
-                map: map
+                icon: icon_path,
+                map: map,
+                content: "<h1><a href='" +
+                    place.url + "'>" + place.title + "</a></h1><p>" + place.description + "</p>" +
+                    "<p>icon: <img src='" + icon_path + "'><strong>" + place.icon + "</strong></p>"
             });
             //クリックイベントで情報ウィンドウを開く
             marker.addListener('click', function() {
+                infowindow.setContent(marker.content);
                 infowindow.open(map, marker);
             });
             //表示位置最適化
--- places.js
+++ places.js
@@ -1,34 +1,38 @@
-var places = getKumamoto();
-//var places = getOsaka();
+//var places = getKumamoto();
+var places = getOsaka();
 
 function getKumamoto() {
     return [{
             "address": "熊本城",
             "title": "熊本城",
-            "icon": "https://img.icons8.com/nolan/64/000000/castle.png",
+            //            "icon": "https://img.icons8.com/nolan/64/000000/castle.png",
+            "icon": "blue-dot",
             "description": "日本三名城のひとつといわれる熊本城は、加藤清正（かとうきよまさ）が慶長6年(1601年)から7年の歳月をかけ築城した天下の名城です。",
-            "url": "https://kumamoto-guide.jp/kumamoto-castle/"
+            "url": "https://kumamoto-guide.jp/kumamoto-castle/",
         },
         {
             "address": "水前寺成趣園",
             "title": "水前寺成趣園",
-            "icon": "https://img.icons8.com/nolan/64/000000/leaf.png",
+            //            "icon": "https://img.icons8.com/nolan/64/000000/leaf.png",
+            "icon": "pink-dot",
             "description": "通称は水前寺公園。豊富な阿蘇伏流水が湧出して作った池を中心にした桃山式回遊庭園で、築山や浮石、芝生、松などの植木で東海道五十三次の景勝を模したといわれる。",
-            "url": "http://www.suizenji.or.jp/"
+            "url": "http://www.suizenji.or.jp/",
         },
         {
             "address": "北岡自然公園",
             "title": "北岡自然公園",
-            "icon": "https://img.icons8.com/nolan/64/000000/leaf.png",
+            //            "icon": "https://img.icons8.com/nolan/64/000000/leaf.png",
+            "icon": "orange-dot",
             "description": "花岡山の麓、細川家の菩提寺・妙解寺跡にある公園。明治4年の廃藩置県後には細川藩主家の本邸（北岡邸）が置かれた。熊本県の重要文化財に指定されている細川家の霊廟があり、文豪・森鴎外の小説のモデルとなった阿部一族の墓もある。園内には、枯山水庭園やバラ園があり市民の憩いの場として親しまれている。",
-            "url": "https://kumamoto-guide.jp/spots/detail/66"
+            "url": "https://kumamoto-guide.jp/spots/detail/66",
         },
         {
             "address": "熊本市役所",
             "title": "熊本市役所",
-            "icon": "https://img.icons8.com/nolan/64/000000/organization.png",
+            //            "icon": "https://img.icons8.com/nolan/64/000000/organization.png",
+            "icon": "purple-dot",
             "description": "花岡山の麓、細川家の菩提寺・妙解寺跡にある公園。明治4年の廃藩置県後には細川藩主家の本邸（北岡邸）が置かれた。熊本県の重要文化財に指定されている細川家の霊廟があり、文豪・森鴎外の小説のモデルとなった阿部一族の墓もある。園内には、枯山水庭園やバラ園があり市民の憩いの場として親しまれている。",
-            "url": "http://www.city.kumamoto.jp/"
+            "url": "http://www.city.kumamoto.jp/",
         }
     ];
 }
@@ -37,28 +41,32 @@ function getOsaka() {
     return [{
             "address": "大阪駅",
             "title": "大阪駅",
-            "icon": "https://img.icons8.com/nolan/64/000000/train.png",
+            //"icon": "https://img.icons8.com/nolan/64/000000/train.png",
+            "icon": "yellow-dot",
             "description": "JR大阪駅構内をご案内します。大阪駅は、1階、中2階、2階、3階で、<br>1階は改札口が「中央口・桜橋口・御堂筋口・御堂筋南口」の4箇所。<br>また、1階の駅出入口は「中央南口・桜橋口・御堂筋北口・御堂筋南口」の4箇所。<br>中2階は2階、1階への連絡通路、<br>2階はホームで1番線から11番線まで。駅出入口の「中央北口」があります。<br>3階には改札口「連絡橋口」が設置されています。",
             "url": "http://osakadesign.com/shinosaka/osaka_konai/index.html"
         },
         {
             "address": "大阪市役所",
             "title": "大阪市役所",
-            "icon": "https://img.icons8.com/nolan/64/000000/home.png",
+            //"icon": "https://img.icons8.com/nolan/64/000000/home.png",
+            "icon": "blue-dot",
             "description": "本庁の入居する大阪市庁舎は、大阪市の中心部、中之島に位置する。御堂筋の東側に位置し、正面には日本銀行大阪支店が位置する。市役所の東側には中之島図書館や大阪市中央公会堂、東洋陶磁美術館がある。",
             "url": "https://www.city.osaka.lg.jp/"
         },
         {
             "address": "通天閣",
             "title": "通天閣",
-            "icon": "https://img.icons8.com/nolan/64/000000/radio-tower.png",
+            //"icon": "https://img.icons8.com/nolan/64/000000/radio-tower.png",
+            "icon": "pink-dot",
             "description": "大阪府大阪市浪速区の新世界中心部に建つ展望塔である。2007年5月15日に、国の登録有形文化財となった[1]。公式キャラクターは「ビリケン」。大阪の観光名所として知られる。",
             "url": "https://www.tsutenkaku.co.jp/"
         },
         {
             "address": "道頓堀",
             "title": "道頓堀",
-            "icon": "https://img.icons8.com/nolan/64/000000/rope-bridge.png",
+            // "icon": "https://img.icons8.com/nolan/64/000000/rope-bridge.png",
+            "icon": "water",
             "description": "松竹座にカニ道楽、そしてグリコの看板などによって象徴される道頓堀は、大阪・ミナミの代表的な繁華街。道頓堀の名は、慶長17年（1612）に私財をなげうって川を開削した安井道頓（やすい・どうとん）の名前に由来。",
             "url": "http://www.dotonbori.or.jp/ja/"
         },
@@ -66,13 +74,15 @@ function getOsaka() {
             "address": "大阪城",
             "title": "大阪城",
             "description": "大阪城は、安土桃山時代に摂津国東成郡生玉荘大坂に築かれ、江戸時代に修築された日本の城。別称は錦城。「大阪城跡」として国の特別史跡に指定されている。",
-            "icon": "https://img.icons8.com/nolan/64/000000/castle.png"
+            //            "icon": "https://img.icons8.com/nolan/64/000000/castle.png"
+            "icon": "blue-pushpin",
         },
         {
             "address": "ユニバーサルスタジオジャパン",
             "title": "ユニバーサルスタジオジャパン",
             "description": "異なる表情を持つさまざまなエリアは、感動がいっぱいの別世界。ハリウッドの超大作映画をテーマにした興奮のライドや人気キャラクターたちのショーなど、子どもから大人まで楽しめる、ワールドクラスのエンターテイメントを集めたテーマパーク。",
-            "icon": "https://img.icons8.com/nolan/64/000000/flash-bang.png",
+            //"icon": "https://img.icons8.com/nolan/64/000000/flash-bang.png",
+            "icon": "grn-pushpin",
             "url": "https://www.usj.co.jp/"
         }
 
